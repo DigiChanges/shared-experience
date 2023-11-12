aa3adc9dda3ac6121c904a03d2aa23e74509ffee
nrusso <nrusso@igichanges.com>
2023-11-12 13:54:42 -0300

    refactor: add compatibility to fastify

--- src/Presentation/Application/AppKoa.ts
+++ src/Presentation/Application/AppKoa.ts
@@ -10,45 +10,61 @@ import { IAppConfig } from './IAppConfig';
 export class AppKoa implements IApp
 {
     public port?: number;
-    private app: Koa;
-    private config: IAppConfig;
-    private server: Server | undefined;
+    #app: Koa;
+    #config: IAppConfig;
+    #server: Server | undefined;
 
     constructor(config: IAppConfig)
     {
         this.port = config.serverPort || 8090;
-        this.app = koaQs(new Koa());
-        this.config = config;
-        this.app.proxy = config.proxy;
+        this.#app = koaQs(new Koa());
+        this.#config = config;
+        this.#app.proxy = config.proxy;
     }
 
-    public addMiddleware<T>(middleware: T): void
+    public async addMiddleware<T>(middleware: T): Promise<void>
     {
-        this.app.use(middleware as Koa.Middleware);
+        this.#app.use(middleware as Koa.Middleware);
     }
 
-    public addRouter<T>(router: T): void
+    public async addRouter<T>(router: T): Promise<void>
     {
-        this.app.use((router as Router).routes());
-        this.app.use((router as Router).allowedMethods());
+        this.#app.use((router as Router).routes());
+        this.#app.use((router as Router).allowedMethods());
     }
 
-    public listen(callback: () => void): Server
+    public async listen(callback: () => void): Promise<void>
     {
-        this.server = this.app.listen(this.port, callback);
-        return this.server;
+        this.#server = this.#app.listen(this.port, callback);
     }
 
-    public callback(): any
+    public async getServer(): Promise<Server | undefined>
     {
-        return this.app.callback();
+        return this.#server;
     }
 
-    close(): void
+    public async callback(): Promise<any>
     {
-        if (this.server)
+        return this.#app.callback();
+    }
+
+    public async setNotFoundHandler<T>(router: any): Promise<void>
+    {
+        this.#app.use((router as Router).routes());
+        this.#app.use((router as Router).allowedMethods());
+    }
+
+    public async setErrorHandler<T>(router: T, options?: unknown): Promise<void>
+    {
+        this.#app.use((router as Router).routes());
+        this.#app.use((router as Router).allowedMethods());
+    }
+
+    public async close(): Promise<void>
+    {
+        if (this.#server)
         {
-            this.server.close();
+            this.#server.close();
         }
     }
 }
